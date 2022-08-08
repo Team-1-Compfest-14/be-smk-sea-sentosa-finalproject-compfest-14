e8646743a32d531a8fee8e0d8041dc15eb52b577
Fajar <fajarmh2@gmail.com>
2022-08-08 21:39:02 +0700

    demo commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,6 @@ import { register } from '../controllers/auth.controller';
 
 const router = Router();
 
-router.post('/v1/auth/register', register);
+router.post('/v1/auth/register/hello', register);
 
 export default router;
