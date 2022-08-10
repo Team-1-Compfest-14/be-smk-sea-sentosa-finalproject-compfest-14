2fb129c7d78e170209440bb47eaeffbaa660289e
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-08-10 21:40:52 +0700

    add routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import Router from 'express';
+import { approvalController } from '../controllers/approval.controller';
 import { authController } from '../controllers/auth.controller';
 
 
 const router = Router();
 
 router.post('/v1/auth/register', authController.register);
+
+router.get('/v1/approval/register', approvalController.getAllNewInstructor);
 
 export default router;
