40fb230836aa1daa7bcc2b450ffd3d342631351e
rahmatsyifana12 <adean435@gmail.com>
2022-08-11 22:05:47 +0700

    Add user role validation

--- src/services/course.service.ts
+++ src/services/course.service.ts
@@ -1,4 +1,6 @@
 import { Course } from '../database/entities/Course';
+import { UserRole } from '../database/entities/User';
+import { Errors } from '../utils/error.util';
 import type { AddCourseType } from '../validations/course.validate';
 import { userService } from './user.service';
 
@@ -6,7 +8,11 @@ class CourseService {
 
     async add(instructorId: number, rawCourse: AddCourseType) {
         const user = await userService.get(instructorId);
-        const course = Course.create({ instructorId: user.id, ...rawCourse });
+        if (user.role !== UserRole.INSTRUCTOR) {
+            throw Errors.NO_PERMISSION;
+        }
+
+        const course = Course.create({ instructorId, ...rawCourse });
 
         await Course.save(course);
     }
