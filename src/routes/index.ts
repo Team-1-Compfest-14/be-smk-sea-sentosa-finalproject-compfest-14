ca8df90b19a3bceb3fefb901a0f3d4ab83f5eab7
rahmatsyifana12 <adean435@gmail.com>
2022-08-11 21:50:17 +0700

    Add add-course controller and routing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import Router from 'express';
 import { authController } from '../controllers/auth.controller';
+import { courseController } from '../controllers/course.controller';
 import {
     courseEnrollmentController
 } from '../controllers/courseEnrollment.controller';
@@ -16,5 +17,8 @@ router.delete('/auth/logout', authenticate('REFRESH'), authController.logout);
 // Course Enrollments
 router.post('/courses/:courseId/enroll', authenticate('ACCESS'),
     courseEnrollmentController.enrollNewCourse);
+
+// Course
+router.post('/courses', authenticate('ACCESS'), courseController.add);
 
 export default router;
