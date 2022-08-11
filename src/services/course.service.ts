780c0d5e223f727cea01a7f255807d652d5c332b
rahmatsyifana12 <adean435@gmail.com>
2022-08-11 21:43:03 +0700

    Add add course service

--- src/services/course.service.ts
+++ src/services/course.service.ts
@@ -1,6 +1,13 @@
+import { Course } from '../database/entities/Course';
+import type { AddCourseType } from '../validations/course.validate';
+
 class CourseService {
 
+    async add(instructorId: number, rawCourse: AddCourseType) {
+        const course = Course.create({ instructorId, ...rawCourse });
 
+        await Course.save(course);
+    }
 
 }
 
