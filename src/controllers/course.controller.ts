3791e4130f542b5cd14a2a0188093ec81cf1f84f
Fajar <fajarmh2@gmail.com>
2022-08-12 23:04:30 +0700

    add new get proposed course and refactor add

--- src/controllers/course.controller.ts
+++ src/controllers/course.controller.ts
@@ -12,12 +12,25 @@ class CourseController {
         const userPayload = await authService.getTokenPayload(req, 'ACCESS');
         const body = validate(req, addCourseSchema, 'body');
 
-        await courseService.add(userPayload!.userId, body);
+        await courseService.add(userPayload!, body);
 
         return sendResponse(res, {
             statusCode: StatusCodes.CREATED,
             success: true,
             message: 'Successfully created a course'
+        });
+    }
+
+    async getProposedCourse(req: Request, res: Response) {
+        const userPayload = await authService.getTokenPayload(req, 'ACCESS');
+
+        const data = await courseService.getNewCourse(userPayload!);
+
+        return sendResponse(res, {
+            statusCode: StatusCodes.CREATED,
+            success: true,
+            data: data,
+            message: 'Successfully gett all proposed course'
         });
     }
 
