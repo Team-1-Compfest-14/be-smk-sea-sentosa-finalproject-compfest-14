dd4f02546ef4f3506401b07546beec5d5a20de94
Fajar <fajarmh2@gmail.com>
2022-08-12 23:58:02 +0700

    modify controller

--- src/controllers/course.controller.ts
+++ src/controllers/course.controller.ts
@@ -4,13 +4,13 @@ import { authService } from '../services/auth.service';
 import { courseService } from '../services/course.service';
 import { sendResponse } from '../utils/api.util';
 import { validate } from '../utils/validate.util';
-import { addCourseSchema } from '../validations/course.validate';
+import { courseParams, courseSchema } from '../validations/course.validate';
 
 class CourseController {
 
     async add(req: Request, res: Response) {
         const userPayload = await authService.getTokenPayload(req, 'ACCESS');
-        const body = validate(req, addCourseSchema, 'body');
+        const body = validate(req, courseSchema, 'body');
 
         await courseService.add(userPayload!, body);
 
@@ -24,7 +24,7 @@ class CourseController {
     async getProposedCourse(req: Request, res: Response) {
         const userPayload = await authService.getTokenPayload(req, 'ACCESS');
 
-        const data = await courseService.getNewCourse(userPayload!);
+        const data = await courseService.getNewCourses(userPayload!);
 
         return sendResponse(res, {
             statusCode: StatusCodes.CREATED,
@@ -34,10 +34,24 @@ class CourseController {
         });
     }
 
-    async getVerifiedCourse(req: Request, res: Response) {
+    async getCourseDetail(req: Request, res: Response) {
+        const userPayload = await authService.getTokenPayload(req, 'ACCESS');
+        const body = validate(req, courseParams, 'params');
+
+        const data = await courseService.getSpecifyCourse(userPayload!, body);
+
+        return sendResponse(res, {
+            statusCode: StatusCodes.CREATED,
+            success: true,
+            data: data,
+            message: 'Successfully get verified course details'
+        });
+    }
+
+    async getVerifiedCourses(req: Request, res: Response) {
         const userPayload = await authService.getTokenPayload(req, 'ACCESS');
 
-        const data = await courseService.getCourse(userPayload!);
+        const data = await courseService.getCourses(userPayload!);
 
         return sendResponse(res, {
             statusCode: StatusCodes.CREATED,
