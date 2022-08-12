203d21ecbd2df58c9b9f57f2f0c4ab1a88e046d9
Fajar <fajarmh2@gmail.com>
2022-08-12 23:35:24 +0700

    add approval service and refactor

--- src/services/approval.service.ts
+++ src/services/approval.service.ts
@@ -4,14 +4,16 @@ import { Errors } from '../utils/error.util';
 import type { ActionType } from '../typings/action';
 import type {
     ApprovalQueryType,
-    ApprovalType
+    ApprovalInstructorType,
+    ApprovalCourseType
 } from '../validations/approval.validate';
+import { Course } from '../database/entities/Course';
 
 class ApprovalService {
 
     async approveOrRejectInstructor(
         { role }: UserPayload,
-        { userId }: ApprovalType,
+        { userId }: ApprovalInstructorType,
         { action }: ApprovalQueryType) {
 
         if (role !== UserRole.ADMIN) {
@@ -24,21 +26,39 @@ class ApprovalService {
         }
 
         const status = await this
-            .authorizationNewInstructor(instructor, action);
+            .authorization(instructor, action);
         return status;
     }
 
-    async authorizationNewInstructor(instructor: User, action: ActionType) {
+    async approveOrRejectProposedCourse(
+        { role }: UserPayload,
+        { courseId }: ApprovalCourseType,
+        { action }: ApprovalQueryType
+    ) {
+        if (role !== UserRole.ADMIN) {
+            throw Errors.NO_PERMISSION;
+        }
+
+        const course = await Course.findOneBy({ id: courseId });
+        if (!course) {
+            throw Errors.COURSE_NOT_FOUND;
+        }
+        const status = await this.authorization(course, action);
+
+        return status;
+    }
+
+    async authorization(object: User | Course, action: ActionType) {
         if (action === 'approve') {
-            instructor.isVerified = true;
-            await instructor.save();
+            object.isVerified = true;
+            await object.save();
 
             return 'approve';
         } else if (action === 'reject') {
-            if (instructor.isVerified === true) {
+            if (object.isVerified === true) {
                 throw Errors.USER_ALREADY_VERIFIED;
             }
-            await instructor.remove();
+            await object.remove();
 
             return 'reject';
         }
