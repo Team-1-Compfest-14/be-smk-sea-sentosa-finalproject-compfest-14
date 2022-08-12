209bd0db4f8ccb5ca0ed0e205c79a1cf6721ed8e
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-08-12 16:49:13 +0700

    add approval action service

--- src/controllers/approval.controller.ts
+++ src/controllers/approval.controller.ts
@@ -2,6 +2,8 @@ import type { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { approvalService } from '../services/approval.service';
 import { sendResponse } from '../utils/api.util';
+import { validate } from '../utils/validate.util';
+import { approvalSchema } from '../validations/approval.validate';
 
 class ApprovalController {
 
@@ -12,6 +14,19 @@ class ApprovalController {
             statusCode: StatusCodes.OK,
             success: true,
             data: listOfInstructor,
+            message: 'Successfully get all new Instructor'
+        });
+    }
+
+    async approvalAction(req: Request, res: Response) {
+        const body = validate(req, approvalSchema, 'body');
+        const query = req.query;
+
+        await approvalService.approveOrRejectInstructor(body, query);
+
+        return sendResponse(res, {
+            statusCode: StatusCodes.OK,
+            success: true,
             message: 'Successfully get all new Instructor'
         });
     }
