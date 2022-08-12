4bd6889ff260ce31631d5065aea225119d56ae8c
Fajar <fajarmh2@gmail.com>
2022-08-12 17:58:44 +0700

    add new validate

--- src/validations/approval.validate.ts
+++ src/validations/approval.validate.ts
@@ -4,7 +4,17 @@ export interface ApprovalType {
     id: number;
 }
 
+export interface ApprovalQueryType {
+    action: string;
+}
+
 export const approvalSchema = joi.object<ApprovalType>({
     id: joi.number()
+        .required()
+});
+
+export const approvalQuerySchema = joi.object<ApprovalQueryType>({
+    action: joi.string()
+        .max(10)
         .required()
 });
