08cc5147acede4593ae5f12ad46759bdc8d663be
Rafi Priatna K <rafipriatnak@gmail.com>
2022-08-13 17:10:15 +0700

    Create an endpoint to add Quiz to module and fix bug cannot add lecture

--- src/controllers/module.controller.ts
+++ src/controllers/module.controller.ts
@@ -5,7 +5,7 @@ import { moduleService } from '../services/module.service';
 import { sendResponse } from '../utils/api.util';
 import { validate } from '../utils/validate.util';
 import {
-    addLectureSchema, courseIdModuleSchema
+    addLectureSchema, courseIdModuleSchema, addQuizSchema
 } from '../validations/module.validate';
 
 class ModuleController {
@@ -13,14 +13,30 @@ class ModuleController {
     async addLecture(req: Request, res: Response) {
         const userPayload = await authService.getTokenPayload(req, 'ACCESS');
         const body = validate(req, addLectureSchema, 'body');
-        const courseId = validate(req, courseIdModuleSchema, 'params');
+        const param = validate(req, courseIdModuleSchema, 'params');
 
-        await moduleService.addLecture(userPayload!.userId, courseId, body);
+        await moduleService.addLecture(userPayload!.userId,
+            param.courseId, body);
 
         return sendResponse(res, {
             statusCode: StatusCodes.CREATED,
             success: true,
             message: 'Successfully created a lecture.'
+        });
+    }
+
+    async addQuiz(req: Request, res: Response) {
+        const userPayload = await authService.getTokenPayload(req, 'ACCESS');
+        const body = validate(req, addQuizSchema, 'body');
+        const param = validate(req, courseIdModuleSchema, 'params');
+
+        await moduleService.addQuiz(userPayload!.userId,
+            param.courseId, body);
+
+        return sendResponse(res, {
+            statusCode: StatusCodes.CREATED,
+            success: true,
+            message: 'Successfully created a quiz.'
         });
     }
 
