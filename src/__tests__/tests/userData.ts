4e5d600929412a34c534f0402978034524d66394
Rafi Priatna K <rafipriatnak@gmail.com>
2022-08-14 11:53:41 +0700

    Update user data for testing

--- src/__tests__/tests/userData.ts
+++ src/__tests__/tests/userData.ts
@@ -1,3 +1,5 @@
+import { UserRole } from '../../../src/database/entities/User';
+
 interface UserDataTestInterface {
     email: string;
     password: string;
@@ -9,19 +11,26 @@ export const userStudentDataTest: UserDataTestInterface = {
     email: 'student-sea-sentosa@gmail.com',
     password: '123456',
     name: 'Sea Sentosa',
-    role: 0
+    role: UserRole.STUDENT
 };
 
-export const userInstructorDataTest: UserDataTestInterface = {
+export const userInstructorNotVerifiedDataTest: UserDataTestInterface = {
     email: 'instructor-sea-sentosa@gmail.com',
     password: '123456',
     name: 'Sea Sentosa Instructor',
-    role: 1
+    role: UserRole.INSTRUCTOR
+};
+
+export const userInstructorVerifiedDataTest: UserDataTestInterface = {
+    email: 'instructor1@gmail.com',
+    password: 'instructor123',
+    name: 'Instructor 1',
+    role: UserRole.INSTRUCTOR
 };
 
 export const userAdminDataTest: UserDataTestInterface = {
-    email: 'admin-sea-sentosa@gmail.com',
-    password: '123456',
-    name: 'Sea Sentosa Admin',
-    role: 2
+    email: 'admin@gmail.com',
+    password: 'admin123',
+    name: 'Admin',
+    role: UserRole.ADMIN
 };
