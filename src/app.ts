d8bea8e0ff3ec7a56acd654e3c8a218b24c2f6f5
rahmatsyifana12 <adean435@gmail.com>
2022-08-21 00:02:55 +0700

    Remove cors origin

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: ['http://localhost:3000',
-    'https://fe-smk-sea-sentosa-finalproject-compfest-14.vercel.app'] }));
+app.use(cors());
 app.use(routes);
 app.use(errorHandling);
 
